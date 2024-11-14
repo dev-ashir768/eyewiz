a407ce872892c47660c16ec287e4d99909408d82
Ashir Arif <techashir167@gmail.com>
2024-11-15 00:03:11 +0500

    images update 4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
     container: {
       center: true,
       padding: {
-        DEFAULT: "1.5rem",
+        DEFAULT: "1rem",
         sm: "2rem",
         md: "3rem",
         lg: "3rem",
