6d897ee2fa8686b94f190d9d0cea7a210bb48dd6
Ashir Arif <techashir167@gmail.com>
2024-11-15 00:05:46 +0500

    images update 5

--- components/ui/site/home/PopularBrands.tsx
+++ components/ui/site/home/PopularBrands.tsx
@@ -32,7 +32,7 @@ const PopularBrands = () => {
                 </div>
                 <div className="lg:flex hidden flex-1 border-t border-[#DBB42C]"></div>
             </div>
-            <div className='grid lg:grid-cols-4 md:grid-cols-3 xs:grid-cols-1 py-12 gap-3'>
+            <div className='grid lg:grid-cols-4 md:grid-cols-3 xs:grid-cols-2 py-12 gap-3'>
                 {
                     brandLogos.map((item, index) => (
                         <div key={index} className='relative h-[120PX] bg-[#9F9F9F] rounded-xl'>
